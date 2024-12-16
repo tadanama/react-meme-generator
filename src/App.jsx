b6748e9d5e468771a4445fb29da8fd5faa3cbb2d
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-16 11:50:31 +0800

    Retrieve memes as soon as App loads and set it to the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
-	return <><h1>Hi</h1></>;
+	// Store all meme data including images from api
+	const [meme, setMeme] = useState();
+
+	// Send get request to api
+	async function getMeme() {
+		try {
+			const response = await axios.get("https://api.imgflip.com/get_memes");
+			setMeme(response.data.data.memes);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	// Run when conponent mounts
+	useEffect(() => {
+		getMeme();
+	}, []);
+	return (
+		<>
+			<h1>hi</h1>
+		</>
+	);
 }
 
 export default App;
