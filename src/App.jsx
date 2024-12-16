55299486923703653b389413b30fd69a1bbc2dd9
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-16 13:44:57 +0800

    Modify the component structure and track the state of the top and bottom input
    Write a function to get a random image from the state when user click button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import MemeText from "./MemeText";
 
 function App() {
 	// Store all meme data including images from api
 	const [meme, setMeme] = useState();
+	// Track the meme input
+	const [memeText, setMemeText] = useState({
+		top: "",
+		bottom: "",
+	});
+	const [imgUrl, setImageUrl] = useState("https://i.imgflip.com/1ur9b0.jpg");
 
 	// Send get request to api
 	async function getMeme() {
@@ -16,13 +23,29 @@ function App() {
 		}
 	}
 
+	function getRandomImage() {
+		const randomNumber = Math.floor(Math.random() * (meme?.length - 1));
+		setImageUrl(meme[randomNumber].url);
+	}
+
 	// Run when conponent mounts
 	useEffect(() => {
 		getMeme();
 	}, []);
 	return (
 		<>
-			<h1>hi</h1>
+			<main className="app">
+				<MemeText
+					memeText={memeText}
+					setMemeText={setMemeText}
+					getRandomImage={getRandomImage}
+				/>
+				<section className="meme-image">
+					<img src={imgUrl} alt="" />
+					<h2 className="top">{memeText?.top}</h2>
+					<h2 className="bottom">{memeText?.bottom}</h2>
+				</section>
+			</main>
 		</>
 	);
 }
